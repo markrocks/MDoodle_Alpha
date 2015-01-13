4df17990aa4b469dfd6adaa49c643fd7646ec530
markrocks <mark@markrocks.com>
2015-01-13 09:18:32 -0500

    committing to mdoodle alpha

---
+++ CocosStoryboard/ImageSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageSelectionViewController.h
+//  MonsterDoodle
+//
+//  Created by Mark Martin on 1/13/15.
+//  Copyright (c) 2015 Sias Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageSelectionViewController : UIViewController
+
+@end
---
+++ CocosStoryboard/ImageSelectionViewController.m
@@ -0,0 +1,37 @@
+//
+//  ImageSelectionViewController.m
+//  MonsterDoodle
+//
+//  Created by Mark Martin on 1/13/15.
+//  Copyright (c) 2015 Sias Studios. All rights reserved.
+//
+
+#import "ImageSelectionViewController.h"
+
+@interface ImageSelectionViewController ()
+
+@end
+
+@implementation ImageSelectionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
