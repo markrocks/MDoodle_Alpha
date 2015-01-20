911fc19336cccef29e4c5e924812b0de23bdba4c
markrocks <mark@markrocks.com>
2015-01-20 00:45:44 -0500

    Started work on load screen

---
+++ CocosStoryboard/ImageLoaderCarouselViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImageLoaderCarouselViewController.h
+//  MonsterDoodle
+//
+//  Created by Mark Martin on 1/20/15.
+//  Copyright (c) 2015 Sias Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FXImageView.h"
+#import "iCarousel.h"
+
+@interface ImageLoaderCarouselViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet iCarousel *carousel;
+
+@end
---
+++ CocosStoryboard/ImageLoaderCarouselViewController.m
@@ -0,0 +1,97 @@
+//
+//  ImageLoaderCarouselViewController.m
+//  MonsterDoodle
+//
+//  Created by Mark Martin on 1/20/15.
+//  Copyright (c) 2015 Sias Studios. All rights reserved.
+//
+
+#import "ImageLoaderCarouselViewController.h"
+
+@interface ImageLoaderCarouselViewController ()
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
+
+@implementation ImageLoaderCarouselViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    //configure carousel
+    _carousel.type = iCarouselTypeCoverFlow2;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
+    {
+        //get image paths
+        NSArray *imagePaths = [[NSBundle mainBundle] pathsForResourcesOfType:@"jpg" inDirectory:@"Bundled Assets"];
+        
+        //preload images (although FXImageView can actually do this for us on the fly)
+        _images = [[NSMutableArray alloc] init];
+        for (NSString *path in imagePaths)
+        {
+            [_images addObject:[UIImage imageWithContentsOfFile:path]];
+        }
+    }
+    return self;
+}
+
+- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel
+{
+    return [_images count];
+}
+
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
+{
+    //create new view if no view is available for recycling
+    if (view == nil)
+    {
+        FXImageView *imageView = [[FXImageView alloc] initWithFrame:CGRectMake(0, 0, 200.0f, 200.0f)];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        imageView.asynchronous = YES;
+        imageView.reflectionScale = 0.5f;
+        imageView.reflectionAlpha = 0.25f;
+        imageView.reflectionGap = 10.0f;
+        imageView.shadowOffset = CGSizeMake(0.0f, 2.0f);
+        imageView.shadowBlur = 5.0f;
+        imageView.cornerRadius = 10.0f;
+        view = imageView;
+    }
+    
+    //show placeholder
+    ((FXImageView *)view).processedImage = [UIImage imageNamed:@"placeholder.png"];
+    
+    //set image
+    ((FXImageView *)view).image = _images[index];
+    
+    return view;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
