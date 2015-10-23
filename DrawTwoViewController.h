2eb7d9c750a10a695c6fff634edff87ee701bde4
Mark Martin <markhmartin@gmail.com>
2015-10-23 00:34:47 -0400

    Resizing stuff
    Worked on resizing — screwed it up — still committing — and then going
    back

---
+++ DrawTwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrawTwoViewController.h
+//  MonsterDoodle
+//
+//  Created by Mark Martin on 10/22/15.
+//  Copyright © 2015 Sias Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawTwoViewController : UIViewController
+
+@end
